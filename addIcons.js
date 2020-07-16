04fb878aba7f1613efef7f725c9ca26f26ede047
Di-homo <68106113+Di-homo@users.noreply.github.com>
2020-07-16 13:24:03 +0300

    change code to Pavel's code

--- addIcons.js
+++ addIcons.js
@@ -1,43 +1,45 @@
 //create massive with icons (name, path, redirectURl)
-var icons = [
-	{
-		name:'twitter',
-		iconPath:'http://dinacheley.com/wialon/lessons/images/twitter-clr.svg',
-		href:'https://twitter.com/WialonGPS'
-	},	
-	{
-		name:'facebook',
-		iconPath:'http://dinacheley.com/wialon/lessons/images/facebook-clr.svg',
-		href:'https://web.facebook.com/Gurtam.en'
-	},
-	{
-		name:'insta',
-		iconPath:'http://dinacheley.com/wialon/lessons/images/insta-clr.svg',
-		href:'https://www.instagram.com/gurtam_team/'
-	},
-	{
-		name:'insta',
-		iconPath:'http://dinacheley.com/wialon/lessons/images/apple-clr.svg',
-		href:'https://apps.apple.com/ru/app/id960699792'
+(function () {
+	// make sure that if an error occurs, it doesn't break other scripts on the page
+	try	{
+		//create array with icons (path, redirectURl)
+		var icons = [
+			{
+				src:'http://dinacheley.com/wialon/lessons/images/twitter-clr.svg',
+				href:'https://twitter.com/WialonGPS'
+			},
+			{
+				src:'http://dinacheley.com/wialon/lessons/images/facebook-clr.svg',
+				href:'https://web.facebook.com/Gurtam.en'
+			},
+			{
+				src:'http://dinacheley.com/wialon/lessons/images/insta-clr.svg',
+				href:'https://www.instagram.com/gurtam_team/'
+			},
+			{
+				src:'http://dinacheley.com/wialon/lessons/images/apple-clr.svg',
+				href:'https://apps.apple.com/ru/app/id960699792'
+			}
+
+		];
+
+		var div = document.createElement('div');
+		icons.forEach(function(icon){
+			div.innerHTML += "<a href='"+ icon.href +"' target='_blank'><img src='"+ icon.src +"'></a>";
+		})
+		div.style = "position: absolute;" +
+			"top: 100%;" +
+			"width: 100%;" +
+			"padding: 2em 0;" +
+			"text-align: center";
+
+		document
+			.getElementsByClassName('login-bg')[0]
+			.insertAdjacentElement('beforeend', div);
 	}
-];
-
-function createDiv(parentNode) {
-	var div = document.createElement('div');
-    div.innerHTML= createLinkIcon(icons);
-    div.style = "border: none;margin:2em auto;display: flex; justify-content: center";
-    document.getElementById(parentNode).after(div);
-
-}
-
-function createLinkIcon(massive){
-	var str = '';
-	for (var i = 0; i < massive.length; i++){
-		var src = massive[i].iconPath;
-		var link = massive[i].href
-		str = str + "<a href='"+link+"' target='_blank'><img src='"+src+"'></a>";
+	catch (err) {
+		// do nothing
+	
 	}
-	return(str);
-}
 
-createDiv('login_body');
+})()
